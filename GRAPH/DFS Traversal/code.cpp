1367b57f604486964ca45421bb1be719749a2e93
Kamal Singh <86668634+kamal-singh819@users.noreply.github.com>
2023-01-14 03:02:17 +0530

    Create code.cpp

---
+++ GRAPH/DFS Traversal/code.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> adj;
+        for(int i=0; i<edges.size(); i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        int adjSiz = adj.size();
+        //int maxSiz = 0;
+        int ans = 0;
+
+        for(auto x : adj){
+            vector<int> temp = x.second;
+            int p = x.first;
+            if(adjSiz-1 == temp.size()){
+                //maxSiz = temp.size();
+                ans = p;
+                break;
+            }
+        }
+        return ans;
+        
+    }
+};
