afe7cf78175f069401fe8db8bd65a8763ea84571
Kamal Singh <86668634+kamal-singh819@users.noreply.github.com>
2023-01-23 03:22:37 +0530

    Create answer.cpp

---
+++ GRAPH/802. Find Eventual Safe States/answer.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool dfsCycle(int s,vector<bool> &visited,vector<bool> &currVisited,vector<bool>&trackCycle,vector<vector<int>>& graph){
+        visited[s] = true;
+        currVisited[s] = true;
+        for(auto x : graph[s]){
+            if(!visited[x]){
+                if(dfsCycle(x,visited,currVisited,trackCycle,graph))
+                    return trackCycle[s] = true;
+            }
+            else if(visited[x] == true && currVisited[x] == true)
+                return trackCycle[s] = true;
+        }
+        currVisited[s] = false;
+        return false;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        vector<int> ans;
+
+        int n = graph.size();
+        vector<bool> visited(n, false);
+        vector<bool> currVisited(n, false);
+        vector<bool> trackCycle(n, false);
+
+        for(int i=0; i<n; i++){
+            if(! visited[i]){
+                dfsCycle(i,visited,currVisited,trackCycle, graph);
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            if(trackCycle[i] == false)
+                ans.push_back(i);
+        }
+
+        return ans;
+        
+    }
+};
