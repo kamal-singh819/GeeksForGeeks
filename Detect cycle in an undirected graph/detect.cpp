e6890d155024f33d16e633447938e1cbfba1e226
Kamal Singh <86668634+kamal-singh819@users.noreply.github.com>
2023-02-10 12:15:44 +0530

    Update detect.cpp

--- Detect cycle in an undirected graph/detect.cpp
+++ Detect cycle in an undirected graph/detect.cpp
@@ -1,9 +1,5 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
 
-// } Driver Code Ends
-/////////////////////////////////////////////////////////////////////////our main code//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////  USING DFS   //////////////////////////////////////////////////////////////////////////
 class Solution {
   private:
     bool dfsSolution(vector<int> adj[], int s, unordered_map<int, bool> &visited, int parent){
@@ -34,27 +30,44 @@ class Solution {
 };
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//{ Driver Code Starts.
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int V, E;
-        cin >> V >> E;
-        vector<int> adj[V];
-        for (int i = 0; i < E; i++) {
-            int u, v;
-            cin >> u >> v;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+
+//////////////////////////       USING BFS        //////////
+class Solution {
+  private:
+    bool bfsCycle(int src, unordered_map<int, bool> &visited, vector<int> adj[]){
+        unordered_map<int, int> parent;
+        parent[src] = -1;
+        visited[src] = true;
+        queue<int> q;
+        q.push(src);
+        while(!q.empty()){
+            int front = q.front();
+            q.pop();
+            for(auto x : adj[front]){
+                if(visited[x] == true && x != parent[front])
+                    return true;
+                
+                else if(!visited[x]){
+                    q.push(x);
+                    visited[x] = true;
+                    parent[x] = front;
+                }
+            }
         }
-        Solution obj;
-        bool ans = obj.isCycle(V, adj);
-        if (ans)
-            cout << "1\n";
-        else
-            cout << "0\n";
+        return false;
+        
     }
-    return 0;
-}
-// } Driver Code Ends
+  public:
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(int V, vector<int> adj[]) {
+        unordered_map<int, bool> visited;
+        for(int i=0; i<V; i++){
+            if(!visited[i]){
+                bool ans = bfsCycle(i, visited, adj);
+                if(ans)
+                    return true;
+            }
+        }
+        return false;
+    }
+};
